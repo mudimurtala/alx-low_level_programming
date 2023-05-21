b95961354acdd8c3d30037c73c1b4be301656a8b
mudimurtala <ibnmuhyideen95@gmail.com>
2023-05-20 23:26:38 -0700

    fine

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+/*
+ * This function prints "Programming is like building a multilingual puzzle to the console
+ */
 int main(void)
 {
 	  puts("\"Programming is like building a multilingual puzzle");
