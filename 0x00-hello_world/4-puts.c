239107377719e1a4425a648a3d16f5809e9eaa53
mudimurtala <ibnmuhyideen95@gmail.com>
2023-05-20 22:40:15 -0700

    fine

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /* To print the below to the console */
 int main(void)
 {
-	  puts("Programming is like building a multilingual puzzle\n");
+	  puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
 
