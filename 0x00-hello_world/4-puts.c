8ac15969268c9ede4ce15d37dbb96b8b6ad94ce4
mudimurtala <ibnmuhyideen95@gmail.com>
2023-05-20 23:43:51 -0700

    fine

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-/*
- * This function prints "Programming is like
-   building a multilingual puzzle to the console
+/**
+ * main - This function prints "Programming is like
+   building a multilingual puzzle to the console.
+ * Return: 0 (Success)  
  */
 int main(void)
 {
