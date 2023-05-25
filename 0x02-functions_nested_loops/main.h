ef8dd58be09936fff88478d0252295c554d9779c
mudimurtala <ibnmuhyideen95@gmail.com>
2023-05-24 22:53:51 -0700

    fine

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,5 +13,18 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 int _putchar(char c);
+extern int _putchar(char c);
+extern void print_alphabet(void);
+extern void print_alphabet_x10(void);
+extern int _islower(int c);
+extern int _isalpha(int c);
+extern int print_sign(int n);
+extern int _abs(int);
+extern int print_last_digit(int);
+extern void jack_bauer(void);
+extern void times_table(void);
+extern int add(int, int);
+extern void print_to_98(int n);
+extern void print_times_table(int n);
 
 #endif
