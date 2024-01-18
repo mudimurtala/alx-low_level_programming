fe6e939f8f0a7fbbb99972fd618cfb6b68d877ab
mudimurtala <ibnmuhyideen95@gmail.com>
2024-01-18 05:19:29 +0100

    c programming

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int add(int a, int b)
+{
+    int sum = 0;
+    sum = a + b;
+    return (sum);
+}
+
+int sub(int a, int b)
+{
+    int difference = 0;
+    difference = a - b;
+    return (difference);
+}
+
+int mul(int a, int b)
+{
+    int product = 0;
+    product = a * b;
+    return (product);
+}
+
+int div(int a, int b)
+{
+    int division = 0;
+    division = a / b;
+    return (division);
+}
+
+int mod(int a, int b)
+{
+    int modulus = 0;
+    modulus = a % b;
+    return (modulus);
+}
